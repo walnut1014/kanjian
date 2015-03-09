102a7e7a4a90a74bc50d4ceb0b09d49e75b37dd3
baiya <lhwhiteteeth@gmail.com>
2015-03-09 12:13:28 +0800

    RequestUtils.java添加上传文件接口

--- app/src/main/java/name/walnut/kanjian/app/utils/RequestUtils.java
+++ app/src/main/java/name/walnut/kanjian/app/utils/RequestUtils.java
@@ -1,12 +1,18 @@
 package name.walnut.kanjian.app.utils;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
+
 import java.util.Map;
 
 import name.walnut.kanjian.app.support.JsonRequestBuilder;
 import name.walnut.kanjian.app.support.RequestQueueContext;
-
-import com.android.volley.Request;
-import com.android.volley.toolbox.JsonObjectRequest;
 
 public class RequestUtils {
 	public static void postJSON(String uri, Map<String, Object> param, DefalutJSONListener listener ){
@@ -16,4 +22,22 @@ public class RequestUtils {
 		
 		RequestQueueContext.INSTANCE.getRequestQueue().add(jsonRequest);
 	}
+
+    /**
+     * @param uri
+     * @param params
+     * @param listener
+     */
+    public static void upload(String uri, RequestParams params, final DefalutJSONListener listener ) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(uri, params, new JsonHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                listener.onResponse(response);
+            }
+
+        });
+    }
 }
