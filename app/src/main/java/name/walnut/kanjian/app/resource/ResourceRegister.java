e3cc03e3962e00670084db7086e71232325b19f2
walnut1014 <walnut1014@gmail.com>
2015-04-26 13:03:07 +0800

    修改过

--- app/src/main/java/name/walnut/kanjian/app/resource/ResourceRegister.java
+++ app/src/main/java/name/walnut/kanjian/app/resource/ResourceRegister.java
@@ -11,53 +11,11 @@ public enum ResourceRegister {
     smsValidateResource("passport/smsvalidate", RequestMethod.POST), //验证验证码
 
     /*-----------------------relation模块-----------------------*/
-    myAccountInitResource("my/accountInit", RequestMethod.GET),
-
-    /**
-     * 获得所有添加好友请求,包括已同意，未同意和待验证
-     * 返回 ｛
-     *          data:[{
-     *              user:{
-     *                  mobilephone:'1300000000',
-     *                  nickName:'刘阿毛' //昵称
-     *              }
-     *              status:0,1,2 //(0发送请求待验证，1接受他人请求，2已建立好友关系)
-     *          }]
-     *       ｝
-     */
-    relationListResource("relation/list", RequestMethod.GET),
-
-    /**
-     * 邀请好友
-     * 请求参数 {id:1}
-     * 返回 {
-     *          success:true
-     *      }
-     */
-    invitFriendResource("relation/invit", RequestMethod.POST),
-
-    /**
-     * 获得所有的好友
-     * 返回 {
-     *    data:[{
-     *              userId:1,
-     *              mobilephone:'1300000000'
-     *              photoCoount: 5 //照片数
-     *          }]
-     * }
-     */
-    friendsResource("relation/friends", RequestMethod.GET),
-
-    /**
-     * 通过手机号查询用户
-     * 参数 {mobilephone:'139000000'}
-     * 返回 [{
-     *          nickName:'' //昵称
-     *          photo
-     *      }]
-     *
-     */
-    queryUserResource("relation/queryUser", RequestMethod.GET)
+    myAccountInitResource("my/accountInit", RequestMethod.GET), //我的账户初始页面，包括昵称，头像
+    relationListResource("relation/list", RequestMethod.GET), //获得所有添加好友请求,包括已同意，未同意和待验证
+    invitFriendResource("relation/invit", RequestMethod.POST),//邀请好友
+    friendsResource("relation/friends", RequestMethod.GET),//获得所有的好友
+    queryUserResource("relation/queryUser", RequestMethod.GET)//通过手机号查询用户
     ;
 
     ResourceRegister(String url, RequestMethod method) {
