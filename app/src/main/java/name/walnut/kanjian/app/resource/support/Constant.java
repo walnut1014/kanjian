66f8b6b539965036fa6a7e575ccd10d8e90e85b4
walnut <walnut1014@gmail.com>
2015-10-20 19:19:15 +0800

    修改稿

--- app/src/main/java/name/walnut/kanjian/app/resource/support/Constant.java
+++ app/src/main/java/name/walnut/kanjian/app/resource/support/Constant.java
@@ -4,5 +4,6 @@ package name.walnut.kanjian.app.resource.support;
  * Created by walnut on 15/10/12.
  */
 public interface Constant {
-    String BASE_URL = "http://192.168.199.203:8080";
+    //String BASE_URL = "http://192.168.199.203:8080";
+    String BASE_URL = "http://120.25.201.172:8080/rest";
 }
